f931fc3ae342c63b6504336b220c36a1874abadc
Thrisha Rajkumar <thrisharajkumar5@gmail.com>
2024-09-28 19:44:47 +0530

    Assignment 1 completed

--- Assignment-1.R
+++ Assignment-1.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #Assignment 1
 # Statistical Computing and Empirical Methods, Data Science MSc
 #Teaching Block 1, 2024
@@ -56,6 +55,146 @@ ls()
 #An author name (for example your name);
 #An output format. Let’s choose HTML.
 
-=======
->>>>>>> d4867d82a6a253d2f5ff34b5dc678ed99943b191
+#QUESTION 5 
+#Matrix Operations
+
+#Use the seq() function to generate a sequence of numbers starting at 12 and decreasing to 2 in
+#steps of -2. Call this vector x vect. You may want to run ?seq or help(seq) to help you do
+#this. The vector x vect should look like this
+
+x_vect <- seq(12, 2, -2)
+
+x_vect
+
+X<- matrix(x_vect, 2,3)
+
+X
+
+Y <- matrix(seq(4), 2,2)
+
+Y
+
+Z <- matrix(seq(4, 10, 2), 2, 2)
+
+Z
+
+t(Y)
+t(Z)
+
+##> x_vect
+#[1] 12 10  8  6  4  2
+#> 
+# > X<- matrix(x_vect, 2,3)
+#> 
+#  > X
+#[,1] [,2] [,3]
+#[1,]   12    8    4
+#[2,]   10    6    2
+#> 
+#  > Y <- matrix(seq(4), 2,2)
+#> 
+#  > Y
+#[,1] [,2]
+#[1,]    1    3
+#[2,]    2    4
+#> 
+#  > Z <- matrix(seq(4, 10, 2), 2, 2)
+#> 
+#  > Z
+#[,1] [,2]
+#[1,]    4    8
+#[2,]    6   10
+#> 
+#  > t(Y)
+#[,1] [,2]
+#[1,]    1    2
+#[2,]    3    4
+#> t(Z)
+#[,1] [,2]
+#[1,]    4    6
+#[2,]    8   10
+
+Sum <- Y+Z
+
+Sum
+
+Mul1 <- Y*Z
+
+Mul1
+
+Mul2 <- Z*Y
+
+Mul2
+
+t(Y)
+
+
+Y <- matrix(seq(4), 2,2)
+X<- matrix(x_vect, 2,3)
+
+#Mul3 <- Y*X
+
+#Mul3
+
+#Matrix element-wise multiplication.
+#A %*% B
+
+A <- matrix(seq(4), 2, 2)
+B <- matrix(seq(4,10, 2), 2, 2)
+A
+B
+#perform element-by-element multiplication
+ABC <- A * B
+
+ABC
+
+XYZ <- A %*% B
+XYZ
+
+#Matrix inverse. Compute the matrix inverse Y
+#−1 via the solve() function:
+  
+  
+solve(Y)
+
+#QUESTION 6
+#6 Writing a function within R
+
+#Within your script create a short function called myFirstRFunc which takes in a single
+#numerical argument n and outputs the sum of all those numbers strictly below n which are
+#divisible by either 2 or 7 or both.
+
+myFristRFunc <- function(n){
+  #numerical argument n and outputs the sum of all those numbers strictly below n which are
+  #divisible by either 2 or 7 or both.
+  # Ensure n is a non-negative integer
+
+stopifnot(is.numeric(n), n %% 1 == 0, n >= 0)
+
+#n = 14
+#sum of n = 2+4+6+7+8+10+12
+
+#creating a vect from n to n-1
+
+vect_n <- seq(1, n - 1)
+
+#filtering numbers that are divisible by 2 0r 7
+
+divisible_numbers <- vect_n[vect_n %% 2 ==0 | vect_n %% 7 ==0]
+
+return(sum(divisible_numbers))
+
+}
+
+
+n<- 14
+result <- myFristRFunc(n)
+
+print(result)
+
+myFristRFunc(1000)
+
+#7 Futher R Markdown exercises
+#8 Version control with RStudio and git
+
 
